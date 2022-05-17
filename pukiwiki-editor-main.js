d6a9c5a4dd1ad6e8a3e2e6d61c08fd52ee53ac6a
panorama32 <mitsueda.itsuki.bc@gmail.com>
2022-05-17 22:33:46 +0900

    fetch preview

--- pukiwiki-editor-main.js
+++ pukiwiki-editor-main.js
@@ -1,6 +1,14 @@
 const body = serializeForm();
 
 $.ajax({
-  url: "",
+  type: "POST",
+  url: "https://ppwww.phys.sci.kobe-u.ac.jp/ppwiki-internal/",
   data: body,
+  headers: {
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Cache-Control": "max-age=0",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Upgrade-Insecure-Requests": "1",
+  },
 });
